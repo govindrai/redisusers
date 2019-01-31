27092b5d1b45be7f6b0e66df8712714723f42fd1
Govind Rai <raigovind93@gmail.com>
2019-01-30 23:44:55 -0800

    Update codebase and fix styling issues

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 // Modules
-const path = require("path"),
-  express = require("express"),
-  bodyParser = require("body-parser"),
-  methodOverride = require("method-override");
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
 
 // Routes
-const users = require("./api/users");
+const users = require('./api/routes/users');
 
 // Local Variables
 const app = express(),
@@ -16,12 +15,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Router Middleware
-app.use("/api/users", users);
+app.use('/api/users', users);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
